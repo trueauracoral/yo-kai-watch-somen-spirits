065d6a5d62b024cc7e3d6928b989770f09ed6c94
zortazert <zortazert@matthewevan.xyz>
2024-05-30 20:30:46 -0500

    snap support

--- scripts/battle.js
+++ scripts/battle.js
@@ -6,13 +6,16 @@ canvas.height = 640;
 const middleWidth = canvas.width / 2;
 const middleHeight = canvas.height / 2;
 
-ctx.fillStyle="black";
+ctx.fillStyle = "black";
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 const middleWheele = loadImage("./images/battle/wheele-middle.png");
-const wheeleImage = loadImage("./images/battle/wheele.png")
+const wheeleImage = loadImage("./images/battle/wheele.png");
 
 let isRotating = false;
+let prevMouseCoords = { x: middleWidth, y: middleHeight };
+let prevRotation = 0;
+const angles = [0, Math.PI / 3, (2 * Math.PI) / 3, Math.PI, (4 * Math.PI) / 3, (5 * Math.PI) / 3, 2 * Math.PI];
 
 function loadImage(src) {
     var img = new Image();
@@ -25,44 +28,40 @@ function getMousePosition(canvas, event) {
     let rect = canvas.getBoundingClientRect();
     let x = event.clientX - rect.left;
     let y = event.clientY - rect.top;
-    return {x: x, y: y};
+    return { x: x, y: y };
 }
 
-//ctx.drawImage(middleWheele, 0, 0);
-
 // https://stackoverflow.com/questions/17411991/html5-canvas-rotate-image
-function drawImageCenter(image, x, y, cx, cy, scale, rotation){
-    ctx.setTransform(scale, 0, 0, scale, x, y); // sets scale and origin
-    // ctx.rotate(0)
+function drawImageCenter(image, x, y, cx, cy, scale, rotation) {
+    ctx.setTransform(scale, 0, 0, scale, x, y);
     ctx.rotate(rotation);
     ctx.drawImage(image, -cx, -cy);
-    ctx.setTransform(1, 0, 0, 1, 0, 0); // reset transform
-} 
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+}
 
-ctx.drawImage(wheeleImage, 0, 0)
-ctx.drawImage(middleWheele, 0 , 0);
-drawImageCenter(wheeleImage, middleWidth, middleHeight, middleWidth, middleHeight, 1, 0);
+ctx.drawImage(wheeleImage, 0, 0);
+ctx.drawImage(middleWheele, 0, 0);
 
-var prevRotation = 0
-function rotateWheel() {
+drawImageCenter(wheeleImage, middleWidth, middleHeight, middleWidth, middleHeight, 1, 60);
+
+function rotateWheel(event) {
     var mouseCoords = getMousePosition(canvas, event);
     console.log(mouseCoords);
     console.log(middleWidth, middleHeight);
-    
+
     var rectCoords = {
         x: mouseCoords.x - middleWidth,
-        y: (mouseCoords.y - middleHeight) * -1
-    }
+        y: mouseCoords.y - middleHeight
+    };
 
     const prevRectCoords = {
         x: prevMouseCoords.x - middleWidth,
-        y: (prevMouseCoords.y - middleHeight) * -1
+        y: prevMouseCoords.y - middleHeight
     };
-    
-    console.log(rectCoords);
-    
+
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.drawImage(middleWheele, 0 , 0);
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(middleWheele, 0, 0);
 
     ctx.strokeStyle = "red";
     ctx.beginPath();
@@ -70,7 +69,7 @@ function rotateWheel() {
     ctx.lineTo(mouseCoords.x, mouseCoords.y);
     ctx.stroke();
     ctx.closePath();
-    
+
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/atan2
     function calcAngle(x, y) {
         return Math.atan2(y, x);
@@ -82,29 +81,52 @@ function rotateWheel() {
 
     prevRotation += angleDifference;
 
-    var rotateCalculuation = Math.atan2(rectCoords.y, rectCoords.x);
-    console.log(rotateCalculuation);
-
     drawImageCenter(wheeleImage, middleWidth, middleHeight, middleWidth, middleHeight, 1, prevRotation);
-    
-    
-    prevMouseCoords = mouseCoords
-};
+    console.log("Image Rotation: ", prevRotation);
 
-let currentMouseCoords = { x: 0, y: 0 };
+    prevMouseCoords = mouseCoords;
+}
+
+function snapToSection() {
+    let closestAngle = angles[0];
+    let minDiff = Math.abs(prevRotation - angles[0]);
+
+    for (let i = 1; i < angles.length; i++) {
+        const diff = Math.abs(prevRotation - angles[i]);
+        if (diff < minDiff) {
+            minDiff = diff;
+            closestAngle = angles[i];
+        }
+    }
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(middleWheele, 0, 0);
+
+    drawImageCenter(wheeleImage, middleWidth, middleHeight, middleWidth, middleHeight, 1, closestAngle);
+    prevRotation = closestAngle;
+}
+
+ctx.drawImage(middleWheele, 0, 0);
+drawImageCenter(wheeleImage, middleWidth, middleHeight, middleWidth, middleHeight, 1, 0);
 
 canvas.addEventListener('pointerdown', (event) => {
     isRotating = true;
     prevMouseCoords = getMousePosition(canvas, event);
-    rotateWheel();
 });
 
 canvas.addEventListener('pointermove', (event) => {
     if (isRotating) {
-        rotateWheel();
+        rotateWheel(event);
     }
 });
 
 canvas.addEventListener('pointerup', () => {
     isRotating = false;
+    snapToSection();
+});
+
+canvas.addEventListener('pointerleave', () => {
+    isRotating = false;
+    snapToSection();
 });
